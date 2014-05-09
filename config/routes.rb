9e5c255ecff47965ff7c0b5f59c39c9700d953f6
Derick Olson <olsonderick@gmail.com>
2014-05-09 14:29:14 -0400

    heroku troubleshooting

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ ShutterLink::Application.routes.draw do
 
   devise_for :users
   
-  get 'welcome/index'
-
   resources :users
   resources :posts
   resources :reviews
   resources :skills
   resources :endorsements
+  resources :welcome
 
+  post "search", to: "application#search"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
