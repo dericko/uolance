badaafa33aaae415e236e364400d98d395694b9d
Derick Olson <olsonderick@gmail.com>
2014-05-09 00:43:47 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShutterLink::Application.config.secret_key_base = 'bbf542eaf40587ec003ff64243a644a6603ce5387557f9eaeba63ad7558646195b78a2788b9b7bdd1cf6a7d3cd05b0034146e941cedf2e4c0884063b740ddf34'
